3ff74099715cf0ee5532016df9710dea67d00744
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-04-26 19:54:30 +0530

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const app = require('./src/app');
 const connectDB = require('./src/db/db');
 require('dotenv').config();
 
+process.on('uncaughtException', (err) => {
+    console.error('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.error(err.name, err.message, err.stack);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.error('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.error(err.name, err.message, err.stack);
+    process.exit(1);
+});
+
 const PORT = process.env.PORT || 5000;
 
 // Connect to Database
