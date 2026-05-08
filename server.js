d07184758a0a96c8d1d46010055ebb93aa873174
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-05-08 17:05:00 +0530

    changes

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
+
+// Replace with your actual Render URL
+const URL = "https://portfolio-backend-95gv.onrender.com";
+
+setInterval(async () => {
+  try {
+    const response = await axios.get(URL);
+    console.log(`Pinged at ${new Date().toISOString()}: Status ${response.status}`);
+  } catch (error) {
+    console.error(`Ping failed at ${new Date().toISOString()}: ${error.message}`);
+  }
+}, 840000); /
+
 const startServer = async () => {
     try {
         // 1. First Connect to Database
